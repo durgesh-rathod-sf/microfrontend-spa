fa0accf0081eed1b6bc71ec99e66351e753acfb8
Megha Verma <megha.verma@sourcefuse.com>
2021-03-03 12:23:39 +0530

    resolved css issue in react by using Kremling

--- my-app-main/src/App.js
+++ my-app-main/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {useCss} from 'kremling'
 import React from "react";
 
 function App({ history }) {
+  const scope = useCss(css)
   return (
     <Router history={history}>
       <Route path="/react">
-        <div className="App">
+        <div className="App" {...scope}>
           <div className="App-Main">
             <p>
               Welcome To React App
@@ -20,3 +21,16 @@ function App({ history }) {
 }
 
 export default App;
+
+const css = `
+& .App-Main {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+`
--- my-app-navigation/src/App.js
+++ my-app-navigation/src/App.js
@@ -1,34 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
+import './app.krem.css';
+import { Scoped } from 'kremling';
 import { Link, withRouter, BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <Scoped css={css}>
         <div className="App">
           <header className="App-header">
             <p>
               Value Cockpit
           </p>
           </header>
-          <div className="nav-bar">
-            <Link to={`/react`} >
-              <p className="nav-item">React</p>
-            </Link>
-            <Link to={`/angular`} >
-              <p className="nav-item">Angular</p>
-            </Link>
-          </div>
-          <div style={{border:"1px solid black"}}>
-            <div id="load-valuecockpit-reactapp"></div>
-            <div id="load-angular-pmo"></div>
+          <div className="wrapper">
+            <div className="nav-bar">
+              <Link to={`/react`} >
+                <p className="nav-item">React</p>
+              </Link>
+              <Link to={`/angular`} >
+                <p className="nav-item">Angular</p>
+              </Link>
+            </div>
+            <div className="project-container">
+              <div id="load-valuecockpit-reactapp"></div>
+              <div id="load-angular-pmo"></div>
+            </div>
           </div>
         </div>
-      </>
+      </Scoped>
     );
   }
 }
 
 export default withRouter(App);
+
+const css = `
+& .App-header {
+  background-color: #282c34;
+  max-height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+& .nav-bar {
+  background-color: #282c34;
+  color: white;
+  height: 700px;
+  width: 200px;
+}
+
+& .nav-item {
+  text-align: left;
+}
+
+& .project-container {
+  border: 1px solid black;
+  flex: 1;
+}
+
+& .wrapper {
+  display: flex;
+}`
