2d77b08d6874b5c8854ff95a9aed8a9d801364d2
Megha Verma <megha.verma@sourcefuse.com>
2021-03-03 13:28:54 +0530

    added internal routing in react project

--- my-app-main/src/App.js
+++ my-app-main/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import {useCss} from 'kremling'
 import React from "react";
 
@@ -7,12 +7,27 @@ function App({ history }) {
   const scope = useCss(css)
   return (
     <Router history={history}>
-      <Route path="/react">
+      <Route path="/react" exact>
         <div className="App" {...scope}>
           <div className="App-Main">
             <p>
-              Welcome To React App
+              Welcome To React Main App
         </p>
+        <Link to={`/react/first-component`}>
+        Render First Component
+      </Link>
+          </div>
+        </div>
+      </Route>
+      <Route path="/react/first-component">
+        <div className="App" {...scope}>
+          <div className="App-Main">
+            <p>
+              Welcome To My First Component
+            </p>
+            <Link to={`/react`}>
+              Back To Main React
+            </Link>
           </div>
         </div>
       </Route>
@@ -24,13 +39,13 @@ export default App;
 
 const css = `
 & .App-Main {
-  background-color: #282c34;
+  background-color: white;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: calc(10px + 2vmin);
-  color: white;
+  color: black;
 }
 `
